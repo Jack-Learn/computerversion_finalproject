a3139d970316fc04561ef6bcd6c675a4df1e33c3
JerryJack121 <73692359+JerryJack121@users.noreply.github.com>
2021-01-10 21:16:19 +0800

    Update

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ def count_area(img, width, height):
     return area
 
 
-width = 1295*2
-height = 746*2
+width = 1295
+height = 746
 imgfold = './dataset/test/sample_good'
 grodtruth_fold = './dataset/test/sample_good_Groundtruth'
 
@@ -26,24 +26,26 @@ time_cost_list = []
 IOU_list = []
 IOU_sum = 0
 time_sum = 0
-
+index_list = ['1', '2', '3', '4', '5', '6', '7', 'average']
 for img in tqdm(range(7)):
     time_start = time.time()
     input_before = cv2.imread(os.path.join(imgfold, (str(img+1) + '_before.jpg')))
     input_after = cv2.imread(os.path.join(imgfold, (str(img+1) + '_after.jpg')))
     answer = cv2.imread(os.path.join(grodtruth_fold, (str(img+1) + '_correct.jpg')))
 
-    input_before = cv2.cvtColor(input_before, cv2.COLOR_BGR2GRAY)   #灰階
-    input_after = cv2.cvtColor(input_after, cv2.COLOR_BGR2GRAY)   #灰階
-    answer = cv2.cvtColor(answer, cv2.COLOR_BGR2GRAY)   #灰階
-
     input_before = cv2.resize(input_before, (width, height), interpolation=cv2.INTER_AREA)   #圖片縮放
     input_after = cv2.resize(input_after, (width, height), interpolation=cv2.INTER_AREA)   #圖片縮放
     answer = cv2.resize(answer, (width, height), interpolation=cv2.INTER_AREA)   #圖片縮放
 
+    input_before = cv2.cvtColor(input_before, cv2.COLOR_BGR2GRAY)   #灰階
+    input_after = cv2.cvtColor(input_after, cv2.COLOR_BGR2GRAY)   #灰階
+    answer = cv2.cvtColor(answer, cv2.COLOR_BGR2GRAY)   #灰階
+    # cv2.imencode('.jpg', input_before)[1].tofile('D:/GitHub/computerversion_finalproject/picture/input_before1.jpg')
+    # cv2.imencode('.jpg', input_after)[1].tofile('D:/GitHub/computerversion_finalproject/picture/input_after1.jpg')
     result = cv2.subtract(input_after, input_before)#圖片相減
-
-    ret, result = cv2.threshold(result, 0,255,cv2.THRESH_OTSU)  #二值化
+    # cv2.imencode('.jpg', result)[1].tofile('D:/GitHub/computerversion_finalproject/picture/result1.jpg')
+    _, result = cv2.threshold(result, 0,255,cv2.THRESH_OTSU)  #二值化
+    # cv2.imencode('.jpg', result)[1].tofile('D:/GitHub/computerversion_finalproject/picture/result2.jpg')
     time_end = time.time()
     time_cost = time_end - time_start
     time_sum += time_cost
@@ -61,13 +63,13 @@ for img in tqdm(range(7)):
     area_or = count_area(bitwise_OR, width, height)
     IOU = (area_and / area_or)*100
     IOU_sum += IOU
-    IOU_list.append('%.2f'%IOU)
+    IOU_list.append('%.2f'%IOU + '%')
     # print(IOU)
     # cv2.waitKey(0)
 
 # average
-IOU_list.append('%.2f'%(IOU_sum/7))
+IOU_list.append('%.2f'%(IOU_sum/7)+'%')
 time_cost_list.append(time_sum/7)
 
-df = pd.DataFrame({'IOU':IOU_list, 'Time':time_cost_list})
+df = pd.DataFrame({'IoU(%)':IOU_list, 'Time(s)':time_cost_list}, index= index_list)
 print(df)
